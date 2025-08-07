edfff1c2c84302fae71d32531af1a1d55e5a4174
sarhan31 <sarhanvohra31@gmail.com>
2025-08-07 14:36:10 +0530

    s0

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ if ('webkitSpeechRecognition' in window) {
     userInput.value += transcript + " ";
   };
    recognition.onend = () => {
-    voiceBtn.innerHTML = 'Speak';
+    voiceBtn.innerHTML = '🎙️';
   };
 
 
